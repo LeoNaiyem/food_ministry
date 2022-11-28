eb2de096ed6bf69c606aa24739d53fbb962d954b
KawserAlam21 <mdnaiyem.islam.10@gmail.com>
2022-11-29 00:58:33 +0600

    screen 6 done

--- accessablitiy/js/asb.js
+++ accessablitiy/js/asb.js
@@ -143,7 +143,6 @@ $(document).ready(function () {
         /**
          * Creating the bar
          */
-        console.log('from accessibility')
         const accessibilityBar = document.createElement("div");
         accessibilityBar.id = "accessibilityBar";
         accessibilityBar.tabindex = "0"
